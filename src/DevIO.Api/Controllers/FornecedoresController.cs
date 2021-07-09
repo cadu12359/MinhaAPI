25784ac99b57be7ef535725fc76b1e901fae4ac8
cadu12359 <csantoslima03@gmail.com>
2021-07-09 13:59:40 -0300

    adicionando camada data e business

---
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,29 @@
+﻿using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/{controller}")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+        }
+
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos() 
+        {
+            var fornecedores = await _fornecedorRepository.ObterTodos();
+
+            return Ok(fornecedores);
+        }
+
+    }
+}
